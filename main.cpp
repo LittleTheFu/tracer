8c88225abbd3babe0bb01950f543970f6f193e77
LittleTheFu <lttlstpd@live.com>
2022-09-19 16:42:14 +0800

    draw two balls

--- main.cpp
+++ main.cpp
@@ -9,21 +9,15 @@ using namespace std;
 
 int main()
 {
-    // const Vector3 origin(0, 0, 0);
-    // const Vector3 dir(1,0,0);
-    // Ray ray(origin, dir);
+    const Vector3 centerOne(5, 5, 30);
+    const Ball ballOne(centerOne, 3);
 
-    // const Vector3 center(10,1,0);
-    // const Ball ball(center, 1);
-
-    // bool hit = ray.hit(ball);
-    // // generate some image
-
-    const Vector3 center(5, 5, 30);
-    const Ball ball(center, 3);
+    const Vector3 centerTwo(-5, -15, 50);
+    const Ball ballTwo(centerTwo, 4);
 
     ObjectPool pool;
-    pool.add(ball);
+    pool.add(ballOne);
+    pool.add(ballTwo);
 
     unsigned width = 512, height = 512;
     const float half_width = width / 2.0f;
@@ -37,10 +31,7 @@ int main()
             const Vector3 dir((x - half_width) / half_width, (y - half_height) / half_height, 2);
             Ray ray(origin, dir);
 
-            // bool hit = ray.hit(ball);
             bool hit = pool.hit(ray);
-            // hit = true;
-            // cout << hit << endl;
 
             image[4 * width * y + 4 * x + 0] = hit ? 255 : 0;
             image[4 * width * y + 4 * x + 1] = 0;
@@ -53,6 +44,5 @@ int main()
     if (error)
         std::cout << "encoder error " << error << ": " << lodepng_error_text(error) << std::endl;
 
-    // cout << hit << endl;
     return 0;
 }
