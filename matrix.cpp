fac1e27e119b9f8d36312e32f804cb3210d4c67a
LittleTheFu <lttlstpd@live.com>
2022-09-25 19:05:14 +0800

    update matrix class

--- matrix.cpp
+++ matrix.cpp
@@ -1,1 +1,92 @@
 #include "matrix.h"
+#include <cmath>
+
+const Matrix Matrix::Identity = Matrix();
+
+Matrix::Matrix()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            m[i][j] = 0;
+        }
+    }
+
+    m[0][0] = 1;
+    m[1][1] = 1;
+    m[2][2] = 1;
+    m[3][3] = 1;
+}
+
+const Matrix Matrix::getScaleMatrix(float sx, float sy, float sz)
+{
+    Matrix mtx;
+
+    mtx.m[0][0] = sx;
+    mtx.m[1][1] = sy;
+    mtx.m[2][2] = sz;
+
+    return mtx;
+}
+
+const Matrix Matrix::getTranslateMatrix(float tx, float ty, float tz)
+{
+    Matrix mtx;
+
+    mtx.m[0][3] = tx;
+    mtx.m[1][3] = ty;
+    mtx.m[2][3] = tz;
+
+    return mtx;
+}
+
+const Matrix Matrix::getRotXMatrix(float theta)
+{
+    Matrix mtx;
+
+    mtx.m[1][1] = std::cos(theta);
+    mtx.m[1][2] = -std::sin(theta);
+    mtx.m[2][1] = std::sin(theta);
+    mtx.m[2][2] = std::cos(theta);
+
+    return mtx;
+}
+const Matrix Matrix::getRotYMatrix(float theta)
+{
+    Matrix mtx;
+
+    mtx.m[0][0] = std::cos(theta);
+    mtx.m[0][2] = std::sin(theta);
+    mtx.m[2][0] = -std::sin(theta);
+    mtx.m[2][2] = std::cos(theta);
+
+    return mtx;
+}
+
+const Matrix Matrix::getRotZMatrix(float theta)
+{
+    Matrix mtx;
+
+    mtx.m[0][0] = std::cos(theta);
+    mtx.m[0][1] = -std::sin(theta);
+    mtx.m[1][0] = std::sin(theta);
+    mtx.m[1][1] = std::cos(theta);
+
+    return mtx;
+}
+
+const Matrix Matrix::Mul(const Matrix &m1, const Matrix &m2)
+{
+    Matrix r;
+
+    for (int i = 0; i < 4; ++i)
+    {
+        for (int j = 0; j < 4; ++j)
+        {
+            r.m[i][j] = m1.m[i][0] * m2.m[0][j] + m1.m[i][1] * m2.m[1][j] +
+                        m1.m[i][2] * m2.m[2][j] + m1.m[i][3] * m2.m[3][j];
+        }
+    }
+    return r;
+}
--- matrix.h
+++ matrix.h
@@ -4,6 +4,18 @@
 class Matrix
 {
 public:
+    Matrix();
+
+    static const Matrix getScaleMatrix(float sx, float sy, float sz);
+    static const Matrix getTranslateMatrix(float tx, float ty, float tz);
+    static const Matrix getRotXMatrix(float theta);
+    static const Matrix getRotYMatrix(float theta);
+    static const Matrix getRotZMatrix(float theta);
+
+    static const Matrix Mul(const Matrix &m1, const Matrix &m2);
+
+    static const Matrix Identity;
+
     float m[4][4];
 };
 
