6c27dab334f3dd381d9b11e8e1c428995fa6778b
LittleTheFu <lttlstpd@live.com>
2022-10-29 18:51:38 +0800

    add color class

---
+++ color.cpp
@@ -0,0 +1,15 @@
+#include "color.h"
+
+Color::Color()
+{
+    this->r = 1;
+    this->g = 1;
+    this->b = 1;
+}
+
+Color::Color(float r, float g, float b)
+{
+    this->r = r;
+    this->g = g;
+    this->b = b;
+}
---
+++ color.h
@@ -0,0 +1,13 @@
+#ifndef _COLOR_H_
+#define _COLOR_H_
+
+class Color
+{
+public:
+    Color();
+    Color(float r, float g, float b);
+    
+    float r, g, b;
+};
+
+#endif
