8f395a0f2ead55fce684f0c245b96bafbf31eb1e
LittleTheFu <lttlstpd@live.com>
2022-10-29 16:53:43 +0800

    remove useless code in object pool class

--- objectpool.h
+++ objectpool.h
@@ -21,13 +21,6 @@ public:
     bool testLightReachable(const Ray &ray, const Vector3 &light);
     bool hitSceneObject(const Ray &ray, float &tMin, int &outIndex, HitInfo &info);
 
-    // bool hit(const Ray &ray, bool &isBall, int &outIndex, Vector3 &hitPoint, Vector3 &hitNormal);
-
-    // void trace(const Ray &ray);
-    // bool startTrace(const Ray &ray, int &index, int maxDepth, float &outFactor);
-    // bool lightTrace(const Ray &ray, int &index, int depth, int maxDepth, float inFactor, float &outFactor);
-    // bool directTrace(const Ray &ray, int &index, HitInfo &outInfo);
-
 private:
     std::vector<Ball> m_balls;
     std::vector<Plane> m_planes;
