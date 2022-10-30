aa44949c42092a89ad648c5dd3113fd6742f0196
LittleTheFu <lttlstpd@live.com>
2022-10-30 09:48:36 +0800

    add some constant color

--- color.cpp
+++ color.cpp
@@ -1,5 +1,11 @@
 #include "color.h"
 
+const Color Color::COLOR_BLACK = Color(0, 0, 0);
+const Color Color::COLOR_WHITE = Color(1, 1, 1);
+const Color Color::COLOR_RED = Color(1, 0, 0);
+const Color Color::COLOR_GREEN = Color(0, 1, 0);
+const Color Color::COLOR_BLUE = Color(0, 0, 1);
+
 Color::Color()
 {
     this->r = 1;
--- color.h
+++ color.h
@@ -6,8 +6,14 @@ class Color
 public:
     Color();
     Color(float r, float g, float b);
-    
+
     float r, g, b;
+
+    const static Color COLOR_BLACK;
+    const static Color COLOR_WHITE;
+    const static Color COLOR_RED;
+    const static Color COLOR_GREEN;
+    const static Color COLOR_BLUE;
 };
 
 #endif
