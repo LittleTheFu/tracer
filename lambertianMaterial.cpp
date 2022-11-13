173684f6527c7612f7330bfa03450072de706360
LittleTheFu <lttlstpd@live.com>
2022-11-13 19:59:07 +0800

    update method of calculating texture color

--- lambertianMaterial.cpp
+++ lambertianMaterial.cpp
@@ -1,5 +1,6 @@
 #include "LambertianMaterial.h"
 #include "lambertian.h"
+#include "common.h"
 
 LambertianMaterial::LambertianMaterial(const Color &rho, const Texture *pTexture)
 {
@@ -15,8 +16,8 @@ Color LambertianMaterial::eval(float u, float v, const Vector3 &wo, Vector3 &wi,
         // f += m_pTexture->getColor(u, v);
         // f /= 5;
 
-        //WARNING: CAST
-        ((Lambertian *)(m_pBrdf))->m_rho = m_pTexture->getColor(u, v);
+        // WARNING: CAST
+        ((Lambertian *)(m_pBrdf))->m_rho = m_pTexture->getColor(u, v) * Common::INV_PI;
     }
 
     Color f = m_pBrdf->sample_f(wo, wi, pdf);
