a0fda1ee9dba38da9a6791a3a0f6381d8068c69f
LittleTheFu <lttlstpd@live.com>
2022-11-13 16:29:10 +0800

    update fish eye camera

--- fishEyeCamera.cpp
+++ fishEyeCamera.cpp
@@ -12,9 +12,15 @@ Ray FishEyeCamera::generateRay(float screenX, float screenY) const
 
     float u = (screenX - half_width) / half_width;
     u = (u + 1) / 2;
+    // u -= 0.5;
+    // if (u < 0)
+    //     u += 1;
 
     float v = (screenY - half_height) / half_height;
     v = (v + 1) / 2;
+    // v -= 0.5;
+    // if (v < 0)
+    //     v += 1;
 
     Ball ball;
     const Vector3 dir = ball.getLocalDirection(u, v);
