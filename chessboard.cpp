d37b67d8d2f7334d656e8282e37b34cc76df3dee
LittleTheFu <lttlstpd@live.com>
2022-11-14 10:40:37 +0800

    update chess board texuture

--- chessboard.cpp
+++ chessboard.cpp
@@ -2,10 +2,15 @@
 
 Color Chessboard::getColor(float u, float v) const
 {
-    int iU = (int)(u / 0.1);
-    int iV = (int)(v / 0.1);
+    int iU = (int)(u / 0.5);
+    int iV = (int)(v / 0.5);
 
-    if (iU % 2 && iV % 2)
+    if (iU == 0 && iV == 0)
+    {
+        return Color::COLOR_WHITE;
+    }
+
+    if (iU == 1 && iV == 1)
     {
         return Color::COLOR_WHITE;
     }
