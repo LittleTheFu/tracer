0ef2fa20808c6c619f3707f0c2cead430b6a5a12
LittleTheFu <lttlstpd@live.com>
2024-10-07 16:45:39 +0800

    update scene objects

--- scene/betaSceneBuilder.cpp
+++ scene/betaSceneBuilder.cpp
@@ -5,13 +5,13 @@ void BetaSceneBuilder::build()
     buildRoom();
     buildLight(Vector3(0, 70, 300), 10);
 
-    buildRedBall(Vector3(70, 70, 350), 20);
-    buildMirrorBall(Vector3(-50, 70, 350), 20);
-    buildGlassBall(Vector3(70, 30, 350), 20);
+    // buildRedBall(Vector3(70, 70, 350), 20);
+    // buildMirrorBall(Vector3(-50, 70, 350), 20);
+    // buildGlassBall(Vector3(70, 30, 350), 20);
     
-    buildRedTri(Vector3(60,35,370));
-    buildGreenTri(Vector3(50,-60,350));
-    buildGlassTri(Vector3(30,35,230));
+    // buildRedTri(Vector3(60,35,370));
+    // buildGreenTri(Vector3(50,-60,350));
+    // buildGlassTri(Vector3(30,35,230));
 
-    // buildBunny(Vector3(10, 10, 350), 500);
+    buildBunny(Vector3(10, 10, 370), 300);
 }
