6b616c57d3b7dd980799817fab32e79d29d0aa4c
LittleTheFu <lttlstpd@live.com>
2024-10-15 16:47:09 +0800

    return a debug color

--- tracer/simpleTracer.cpp
+++ tracer/simpleTracer.cpp
@@ -17,7 +17,7 @@ Color SimpleTracer::trace(const ObjectPool *pool,
     bool isLightHit = false;
     if (!pool->hitSceneWithLight(ray, record, isLightHit))
     {
-        return Color::COLOR_BLACK;
+        return Color::COLOR_YELLOW;
     }
 
     if(isLightHit)
