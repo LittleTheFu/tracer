4a4702cd04d6acb70841540c588e6150d018d102
LittleTheFu <lttlstpd@live.com>
2024-11-06 12:26:49 +0800

    remove mathConstantDef.cpp

--- common/mathConstantDef.h
+++ common/mathConstantDef.h
@@ -1,19 +1,23 @@
 #ifndef _MATH_CONSTANT_DEF_H_
 #define _MATH_CONSTANT_DEF_H_
 
+
+#include "mathConstantDef.h"
+#include <limits>
+
 class MathConstant
 {
 public:
-    const static float PI;
-    const static float TWO_PI;
-    const static float FOUR_PI;
-    const static float INV_PI;
-    const static float INV_TWO_PI;
+    inline const static float PI = 3.14159274101257324219f;
+    inline const static float TWO_PI = 2 * PI;;
+    inline const static float FOUR_PI = 4 * PI;
+    inline const static float INV_PI = 1 / PI;
+    inline const static float INV_TWO_PI = 1 / TWO_PI;
 
-    const static float FLOAT_SAMLL_NUMBER;
-    const static float FLOAT_MAX;
-    const static float FLOAT_POSITIVE_INFINITY;
-    const static float FLOAT_NEGETIVE_INFINITY;
+    inline const static float FLOAT_SAMLL_NUMBER = 0.0001f;
+    inline const static float FLOAT_MAX = std::numeric_limits<float>::max();
+    inline const static float FLOAT_POSITIVE_INFINITY = std::numeric_limits<float>::infinity();
+    inline const static float FLOAT_NEGETIVE_INFINITY = -std::numeric_limits<float>::infinity();
 };
 
 #endif
