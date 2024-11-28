9f4fc36aee88b8e883eb53c4b2f1bd1786e5aaa6
LittleTheFu <lttlstpd@live.com>
2024-11-28 14:44:06 +0800

    update scene objects

--- scene/betaSceneBuilder.cpp
+++ scene/betaSceneBuilder.cpp
@@ -10,9 +10,13 @@ void BetaSceneBuilder::build()
     buildRedBall(Vector3(-80, 0, 350), 20);
     buildAquaBall(Vector3(-70, 70, 250), 20);
     buildMirrorBall(Vector3(-50, 70, 350), 20);
+
+    //to be fixed : light power leak
     buildGlassBall(Vector3(60, 30, 300), 20);
     
     buildRedTri(Vector3(55,-20,370));
     buildGreenTri(Vector3(50,-60,350));
-    buildGlassTri(Vector3(20,35,230));
+
+    //to be fixed : light power leak
+    // buildGlassTri(Vector3(20,35,230));
 }
