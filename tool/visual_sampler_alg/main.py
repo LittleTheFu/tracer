61d49076d0a05d0994bf6c58530c95f7f38a185c
LittleTheFu <lttlstpd@live.com>
2024-11-28 13:42:17 +0800

    test python lib

--- tool/visual_sampler_alg/main.py
+++ tool/visual_sampler_alg/main.py
@@ -1,1 +1,19 @@
-print("Hello, World!")
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+
+# 创建数据
+x = np.linspace(-5, 5, 100)
+y = np.linspace(-5, 5, 100)
+x, y = np.meshgrid(x, y)
+z = np.sin(np.sqrt(x**2 + y**2))
+
+# 创建图形
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# 绘制三维曲面
+ax.plot_surface(x, y, z, cmap='viridis')
+
+# 显示图形
+plt.show()
