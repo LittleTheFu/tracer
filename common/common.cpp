6f9d618d8d64886366093dcb3f72df8f425d99ff
LittleTheFu <lttlstpd@live.com>
2024-11-30 12:51:22 +0800

    fix warning

--- common/common.cpp
+++ common/common.cpp
@@ -19,9 +19,22 @@ Vector3 Common::getNormal(const Vector3 &from, const Vector3 &to)
 void Common::printCurrentTime()
 {
     std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
-
     std::time_t now_time_t = std::chrono::system_clock::to_time_t(now);
-    std::tm local_tm = *std::localtime(&now_time_t);
+    std::tm local_tm;
+
+#if defined(_WIN32)
+    if (localtime_s(&local_tm, &now_time_t) != 0)
+    {
+        std::cerr << "Error calling localtime_s" << std::endl;
+        return;
+    }
+#else
+    if (localtime_r(&now_time_t, &local_tm) == nullptr)
+    {
+        std::cerr << "Error calling localtime_r" << std::endl;
+        return;
+    }
+#endif
 
     std::cout << "Current time: " << std::put_time(&local_tm, "%H:%M:%S") << std::endl;
 }
