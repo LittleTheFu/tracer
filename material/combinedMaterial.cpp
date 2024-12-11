d734dd3fa48b9038754ead66cdb574689bdc56f5
LittleTheFu <lttlstpd@live.com>
2024-12-11 16:16:42 +0800

    update combined Material

--- material/combinedMaterial.cpp
+++ material/combinedMaterial.cpp
@@ -1,1 +1,42 @@
 #include "combinedMaterial.h"
+#include "mathUtility.h"
+#include <cassert>
+#include <memory>
+
+CombinedMaterial::CombinedMaterial(std::shared_ptr<const Material> first, std::shared_ptr<const Material> second)
+{
+    assert(first && second);
+
+    m_factor = 0.5f;
+
+    m_FirstMaterial = first;
+    m_SecondMaterial = second;
+
+    m_currentMaterial = m_FirstMaterial;
+}
+
+bool CombinedMaterial::isMirror() const
+{
+    if (m_currentMaterial)
+        return m_currentMaterial->isMirror();
+
+    return false;
+}
+
+bool CombinedMaterial::isDelta() const
+{
+    if (m_currentMaterial)
+        return m_currentMaterial->isDelta();
+
+    return false;
+}
+
+Color CombinedMaterial::eval(float u, float v, const Vector3 &wo, Vector3 &wi, float &pdf) const
+{
+    // if (MathUtility::genRandomDecimal() < m_factor)
+    //     m_currentMaterial = m_FirstMaterial;
+    // else
+    //     m_currentMaterial = m_SecondMaterial;
+
+    return Color();
+}
