21b94407bc3c0cdffddfa879ff0f4d3ccc465bfb
LittleTheFu <lttlstpd@live.com>
2024-12-30 11:35:30 +0800

    test config

--- common/config.cpp
+++ common/config.cpp
@@ -3,8 +3,8 @@
 //don't use ROOM_VOLUME right now
 SceneType configScene = SceneType::ROOM_TEAPOT;
 
-int configResolutionScale = 10;
-int configSamplersPerPixel = 40;
+int configResolutionScale = 1;
+int configSamplersPerPixel = 1;
 int configNeeTracerDepth = 10;
 
 std::string configOutputImageName = "img";
