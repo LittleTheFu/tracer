4e24c77548634a026ff81ec02c22f59e85d54b2a
LittleTheFu <lttlstpd@live.com>
2024-12-30 10:29:15 +0800

    save

--- main/main.cpp
+++ main/main.cpp
@@ -1,10 +1,31 @@
+#include <string>
 #include <memory>
 
 #include "config.h"
 #include "scene.h"
+#include "sceneDef.h"
 
-int main()
+int main(int argc, char *argv[])
 {
+    if (argc >= 2)
+    {
+        std::string str = argv[1];
+        if (str == "simple_room")
+            configScene = SceneType::ROOM_SIMPLE;
+        else if (str == "simple_room_bunny")
+            configScene = SceneType::ROOM_SIMPLE_BUNNY;
+        else if (str == "complex_room_bunny")
+            configScene = SceneType::ROOM_COMPLEX_BUNNY;
+        else if (str == "room_teapot")
+            configScene = SceneType::ROOM_TEAPOT;
+        else if (str == "room_material_balls")
+            configScene = SceneType::ROOM_MATERIAL_BALLS;
+        else
+        {
+            // do nothing
+        }
+    }
+
     Scene scene(configScene, configResolutionScale, configSamplersPerPixel, configNeeTracerDepth);
     scene.run();
 
