b1797d8414f99f5e26f92afc7c867f45152d21cd
LittleTheFu <lttlstpd@live.com>
2024-12-30 10:33:17 +0800

    add script to run multiple tracer scenes

---
+++ document/run_tracer_multiple.sh
@@ -0,0 +1,16 @@
+#!binbash
+
+# 设置可执行文件的路径
+EXECUTABLE=.binray_tracer
+
+# 定义要传递的参数数组
+SCENES=(simple_room simple_room_bunny complex_room_bunny room_teapot room_material_balls)
+
+# 循环运行可执行文件并传递不同的参数
+for SCENE in ${SCENES[@]}; do
+    echo 正在运行场景 $SCENE
+    $EXECUTABLE $SCENE
+    echo 场景 $SCENE 运行完成
+done
+
+echo 所有场景运行完成
