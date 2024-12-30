cb2319fa47f8fa3847d44700df71db18151206c3
LittleTheFu <lttlstpd@live.com>
2024-12-30 10:35:36 +0800

    add scene configuration for additional room types

--- main/main.cpp
+++ main/main.cpp
@@ -11,15 +11,30 @@ int main(int argc, char *argv[])
     {
         std::string str = argv[1];
         if (str == "simple_room")
+        {
             configScene = SceneType::ROOM_SIMPLE;
+            configOutputImageName = "simple_room";
+        }
         else if (str == "simple_room_bunny")
+        {
             configScene = SceneType::ROOM_SIMPLE_BUNNY;
+            configOutputImageName = "simple_room_bunny";
+        }
         else if (str == "complex_room_bunny")
+        {
             configScene = SceneType::ROOM_COMPLEX_BUNNY;
+            configOutputImageName = "complex_room_bunny";
+        }
         else if (str == "room_teapot")
+        {
             configScene = SceneType::ROOM_TEAPOT;
+            configOutputImageName = "room_teapot";
+        }
         else if (str == "room_material_balls")
+        {
             configScene = SceneType::ROOM_MATERIAL_BALLS;
+            configOutputImageName = "room_material_balls";
+        }
         else
         {
             // do nothing
