d96731b9f9ccea3931562e475b7d1c5d98f9e54e
LittleTheFu <lttlstpd@live.com>
2024-12-31 10:53:16 +0800

    refactor main function to separate configuration logic into configFromParams

--- main/main.cpp
+++ main/main.cpp
@@ -5,41 +5,52 @@
 #include "scene.h"
 #include "sceneDef.h"
 
+void configFromParams(int argc, char *argv[])
+{
+    if (argc < 2)
+        return;
+
+    if (!argv)
+        return;
+
+    if (!argv[1])
+        return;
+
+    std::string str = argv[1];
+    if (str == "simple_room")
+    {
+        configScene = SceneType::ROOM_SIMPLE;
+        configOutputImageName = "simple_room";
+    }
+    else if (str == "simple_room_bunny")
+    {
+        configScene = SceneType::ROOM_SIMPLE_BUNNY;
+        configOutputImageName = "simple_room_bunny";
+    }
+    else if (str == "complex_room_bunny")
+    {
+        configScene = SceneType::ROOM_COMPLEX_BUNNY;
+        configOutputImageName = "complex_room_bunny";
+    }
+    else if (str == "room_teapot")
+    {
+        configScene = SceneType::ROOM_TEAPOT;
+        configOutputImageName = "room_teapot";
+    }
+    else if (str == "room_material_balls")
+    {
+        configScene = SceneType::ROOM_MATERIAL_BALLS;
+        configOutputImageName = "room_material_balls";
+    }
+    else
+    {
+        // do nothing
+    }
+}
+
 int main(int argc, char *argv[])
 {
-    if (argc >= 2)
-    {
-        std::string str = argv[1];
-        if (str == "simple_room")
-        {
-            configScene = SceneType::ROOM_SIMPLE;
-            configOutputImageName = "simple_room";
-        }
-        else if (str == "simple_room_bunny")
-        {
-            configScene = SceneType::ROOM_SIMPLE_BUNNY;
-            configOutputImageName = "simple_room_bunny";
-        }
-        else if (str == "complex_room_bunny")
-        {
-            configScene = SceneType::ROOM_COMPLEX_BUNNY;
-            configOutputImageName = "complex_room_bunny";
-        }
-        else if (str == "room_teapot")
-        {
-            configScene = SceneType::ROOM_TEAPOT;
-            configOutputImageName = "room_teapot";
-        }
-        else if (str == "room_material_balls")
-        {
-            configScene = SceneType::ROOM_MATERIAL_BALLS;
-            configOutputImageName = "room_material_balls";
-        }
-        else
-        {
-            // do nothing
-        }
-    }
+    configFromParams(argc, argv);
 
     Scene scene(configScene, configResolutionScale, configSamplersPerPixel, configNeeTracerDepth);
     scene.run();
