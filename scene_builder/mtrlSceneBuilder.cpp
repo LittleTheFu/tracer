d85f3661565376b0dfbf6b30dcdbe81d21a35f07
LittleTheFu <lttlstpd@live.com>
2025-01-01 15:05:19 +0800

    update rooms

--- scene_builder/mtrlSceneBuilder.cpp
+++ scene_builder/mtrlSceneBuilder.cpp
@@ -3,16 +3,16 @@
 void MtrlSceneBuilder::build()
 {
     buildRoom();
-    buildLight(Vector3(0,80,160), 16);
+    buildLight(Vector3(0,70,160), 23);
     setLightIntensity(80.0f);
 
     buildMeasuredGreenBall(Vector3(-70, -30, 280), 20);
-    buildMeasuredWhiteBall(Vector3(-30, -30, 280), 20);
-    buildMeasuredMetalBall(Vector3(30, -30, 280), 20);
+    buildMeasuredWhiteBall(Vector3(-25, -30, 280), 20);
+    buildMeasuredMetalBall(Vector3(25, -30, 280), 20);
     buildMeasuredMintBall(Vector3(70, -30, 280), 20);
     
     buildMeasuredGreenPvcBall(Vector3(-70, 30, 280), 20);
-    buildMeasuredBrushedAluminiumBall(Vector3(-30, 30, 280), 20);
-    buildMeasuredTarkinTunicBall(Vector3(30, 30, 280), 20);
+    buildMeasuredBrushedAluminiumBall(Vector3(-25, 30, 280), 20);
+    buildMeasuredTarkinTunicBall(Vector3(25, 30, 280), 20);
     buildMeasuredSariSilkBall(Vector3(70, 30, 280), 20);
 }
--- scene_builder/teapotSceneBuilder.cpp
+++ scene_builder/teapotSceneBuilder.cpp
@@ -5,7 +5,7 @@
 void TeapotSceneBuilder::build()
 {
     buildRoom();
-    buildLight(Vector3(0,30,200), 16);
+    buildLight(Vector3(0,30,200), 20);
     setLightIntensity(80.0f);
     
     buildBunny(Vector3(-10, -60, 300), 24, ResourceDef::TEAPOT, MATERIAL_TYPE::M_MEASURED_TARKIN_TUNIC_BRDF);
