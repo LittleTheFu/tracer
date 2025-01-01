6be297576dbe72cffd6d3023e91a85902b2b97c7
LittleTheFu <lttlstpd@live.com>
2025-01-01 18:30:54 +0800

    update scene objects

--- common/config.cpp
+++ common/config.cpp
@@ -1,10 +1,10 @@
 #include "config.h"
 
 //don't use ROOM_VOLUME right now
-SceneType configScene = SceneType::ROOM_TEAPOT;
+SceneType configScene = SceneType::ROOM_SIMPLE;
 
-int configResolutionScale = 4;
-int configSamplersPerPixel = 10;
+int configResolutionScale = 2;
+int configSamplersPerPixel = 2;
 int configNeeTracerDepth = 10;
 
 std::string configOutputImageName = "img";
