febb69e6374ceba1b52c832d2370c2a8f567cac2
LittleTheFu <lttlstpd@live.com>
2025-01-07 09:54:49 +0800

    update simple bunny material

--- scene_builder/simpleBunnySceneBuilder.cpp
+++ scene_builder/simpleBunnySceneBuilder.cpp
@@ -8,7 +8,7 @@ void SimpleBunnySceneBuilder::build()
     buildLight(Vector3(40, 40, 350), 20);
     setLightIntensity(40.0f);
     
-    buildBunny(Vector3(30, -100, 350), 900, ResourceDef::SIMPLE_BUNNY, MATERIAL_TYPE::M_DIELECTRIC);
+    buildBunny(Vector3(30, -100, 350), 900, ResourceDef::SIMPLE_BUNNY, MATERIAL_TYPE::M_MEASURED_SARI_SILK_BRDF);
 }
 
 MATERIAL_TYPE SimpleBunnySceneBuilder::getCeilingMaterial() const
