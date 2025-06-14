137c01778429441f54d18fe74ab3796c41459f21
LittleTheFu <lttlstpd@live.com>
2025-06-14 15:38:22 +0800

    read measured bxdf from disk only once

--- bxdf/materialPVC.cpp
+++ bxdf/materialPVC.cpp
@@ -8,8 +8,11 @@ MaterialPVC::MaterialPVC()
 
 std::unique_ptr<Bsdf> MaterialPVC::createBsdf(const Interaction &interaction)
 {
+    //quick and dirty,maybe in the future we will create a manager
+    static std::shared_ptr<MeasuredBxdf> measuredBxdf = std::make_shared<MeasuredBxdf>(ResourceDef::BRDF_SARI_SILK);
+
     std::unique_ptr<Bsdf> bsdf = std::make_unique<Bsdf>(interaction.normal_shading);
-    bsdf->addBxdf(std::make_shared<MeasuredBxdf>(ResourceDef::BRDF_GREEN_PVC));
+    bsdf->addBxdf(measuredBxdf);
 
     return bsdf;
 };
