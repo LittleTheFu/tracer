73afe0cdb2660ff80e0072e339f1fa247910295f
LittleTheFu <lttlstpd@live.com>
2025-06-17 14:48:57 +0800

    test texture albedo

--- common/config.cpp
+++ common/config.cpp
@@ -1,10 +1,10 @@
 #include "config.h"
 
 //don't use ROOM_VOLUME right now
-SceneType configScene = SceneType::ROOM_SIMPLE;
+SceneType configScene = SceneType::ROOM_TEAPOT;
 
-int configResolutionScale = 10;
-int configSamplersPerPixel = 100;
+int configResolutionScale = 1;
+int configSamplersPerPixel = 1;
 int configNeeTracerDepth = 10;
 
 std::string configOutputImageName = "img";
