91ce96a2b965d41b0dd8b32617b775b23bd423d9
LittleTheFu <lttlstpd@live.com>
2025-07-05 18:45:03 +0800

    give user a chance to select model material

--- common/config.cpp
+++ common/config.cpp
@@ -1,7 +1,7 @@
 #include "config.h"
 
 //don't use ROOM_VOLUME right now
-SceneType configScene = SceneType::ROOM_BOT;
+SceneType configScene = SceneType::ROOM_TEAPOT;
 
 int configResolutionScale = 1;
 int configSamplersPerPixel = 2;
