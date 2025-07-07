a10c1b31e74fc2fdbb4a322fca9ad99d3bbcf1b1
LittleTheFu <lttlstpd@live.com>
2025-07-07 21:39:16 +0800

    UPDATE conell box

--- common/config.cpp
+++ common/config.cpp
@@ -3,8 +3,8 @@
 //don't use ROOM_VOLUME right now
 SceneType configScene = SceneType::ROOM_CORNELL;
 
-int configResolutionScale = 4;
-int configSamplersPerPixel = 50;
+int configResolutionScale = 1;
+int configSamplersPerPixel = 3;
 int configNeeTracerDepth = 6;
 
 std::string configOutputImageName = "img";
