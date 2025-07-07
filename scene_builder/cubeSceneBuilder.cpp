1b8b01ffe6c0569b6e97940f587cede43fbbb86b
LittleTheFu <lttlstpd@live.com>
2025-07-07 14:52:14 +0800

    update cube scene

--- scene_builder/cubeSceneBuilder.cpp
+++ scene_builder/cubeSceneBuilder.cpp
@@ -5,7 +5,7 @@
 void CubeSceneBuilder::build()
 {
     buildRoom();
-    buildLight(Vector3(0, 30, 220), 5);
+    buildLight(Vector3(0, 30, 180), 12);
     setLightIntensity(0, 160.0f);
     // setLightIntensity(1, 30.0f);
     // setLightIntensity(2, 30.0f);
@@ -13,11 +13,11 @@ void CubeSceneBuilder::build()
     buildModel(Vector3(-10, 0, 300),
                30,
                ResourceDef::SLAB,
-               MATERIAL_TYPE::M_AQUA,
+               MATERIAL_TYPE::M_YELLOW,
                false,
                true);
 
-    buildModel(Vector3(-10, 0, 280),
+    buildModel(Vector3(10, 0, 220),
                20,
                ResourceDef::SLAB,
                MATERIAL_TYPE::M_GLASS,
