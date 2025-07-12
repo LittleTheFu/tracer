fba446e76e2d06e06b8cc168279d6d5e06aed836
LittleTheFu <lttlstpd@live.com>
2025-07-12 15:09:44 +0800

    update render config

--- common/config.cpp
+++ common/config.cpp
@@ -3,7 +3,7 @@
 //don't use ROOM_VOLUME right now
 SceneType configScene = SceneType::ROOM_VOLUME;
 
-int configResolutionScale = 10;
+int configResolutionScale = 40;
 int configSamplersPerPixel = 1024;
 int configNeeTracerDepth = 10;
 float configLightIntensityScale = 50.0f;
